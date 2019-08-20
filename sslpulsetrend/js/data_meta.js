c8add3c3429d58c0e5ac7a388825d138eee0d845
Kenji Urushima <kenji.urushima@gmail.com>
2019-08-20 23:06:51 +0900

    201908 update

--- sslpulsetrend/js/data_meta.js
+++ sslpulsetrend/js/data_meta.js
@@ -1,1 +1,1 @@
-var sslPulseMetadata={"begin":{"value":1335190374,"string":"2012 Apr 23","string_jp":"2012年4月23日"},"end":{"value":1559549549,"string":"2019 Jun 3","string_jp":"2019年6月3日"}};
+var sslPulseMetadata={"begin":{"value":1335190374,"string":"2012 Apr 23","string_jp":"2012年4月23日"},"end":{"value":1564809781,"string":"2019 Aug 3","string_jp":"2019年8月3日"}};
