f96336bf047d5da944efc86c8f3b4244baa90b93
Kenji Urushima <kenji.urushima@gmail.com>
2022-01-17 22:30:47 +0900

    2022 Jan update

--- sslpulsetrend/js/data_meta.js
+++ sslpulsetrend/js/data_meta.js
@@ -1,1 +1,1 @@
-var sslPulseMetadata={"begin":{"value":1335190374,"string":"2012 Apr 23","string_jp":"2012年4月23日"},"end":{"value":1639478588,"string":"2021 Dec 14","string_jp":"2021年12月14日"}};
+var sslPulseMetadata={"begin":{"value":1335190374,"string":"2012 Apr 23","string_jp":"2012年4月23日"},"end":{"value":1641308612,"string":"2022 Jan 5","string_jp":"2022年1月5日"}};
